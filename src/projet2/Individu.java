70d9d53d360c817268acf4f83efc093421d9e2c4
Sherman-1 <herman.simon.lm@gmail.com>
2022-03-22 12:15:01 +0100

    Projet 2 première ébauche

--- src/projet2/Individu.java
+++ src/projet2/Individu.java
@@ -2,11 +2,12 @@ package projet2;
 
 public class Individu {
 
-    protected boolean contamine, vaccine;
-    protected int x,y; // coordonnées
-    protected double coeff_contamination;
+    protected boolean contamine, etat_futur; // Noémie A1 en anglais
+    protected boolean vaccine;
+    protected int etatEntier;
+    protected int x, y; // coordonnées
 
-    public Individu(int x, int y,boolean cont, boolean vac) {
+    public Individu(int x, int y, boolean cont, boolean vac) {
 
         this.x = x;
         this.y = y;
@@ -15,7 +16,37 @@ public class Individu {
 
     }
 
+    void defConta(boolean state) {
 
+        this.contamine = state;
+    }
 
+    void defFutur(boolean futur) {
+
+        this.etat_futur = futur;
+    }
+
+    void changeEtatEntier() {
+
+        if (this.contamine == false && this.vaccine == false) {
+
+            this.etatEntier = 0;
+        }
+
+        else if (this.contamine == false && this.vaccine == true) {
+
+            this.etatEntier = 1;
+        }
+
+        else if (this.contamine == true && vaccine == false) {
+
+            this.etatEntier = 2;
+        }
+
+        else {
+
+            this.etatEntier = 3;
+        }
+    }
 
 }
