da91d8144987ab31309e7acc456f4da9ea4e222a
Sherman-1 <herman.simon.lm@gmail.com>
2022-03-22 10:10:48 +0100

    Version finale Projet 1

--- src/mini1.java
+++ src/mini1.java
@@ -1,41 +1,144 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class mini1 {
+    public static void main(String[] args) throws InterruptedException {
+        int ligne = 20, colonne = 20;
 
-    void run(int[] self, int nombreTours, int delai) {
-        /*Méthode principale du jeu.
-        Fait tourner le jeu de la vie pendant nombreTours.
-        Elle rafraichit l'affichage à chaque tour et attend delai enter chaque tour.
-        :param nombreTours: nomre de tours à effectuer
-        :param delai: temps d'attente en secondes entre chaque tour*/
-        for(int k=0;k<nombreTours;k++){ //on va faire nombreTours tours
-            /*JeuDelaVie([]) //à définir le JeuDeLaVie
-            PAS DU TOUT FINI*/
+        // 0 : mort     1 : vie
+        // état de départ des cellules
+        int[][] tableau = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
 
+        //  TEST
+        int grille[][] = new int[40][40];
+        Arrays.stream(grille).forEach(i -> Arrays.fill(i, 0));
+        //
+
+        JFrame p = panneau(ligne);
+        Afficher(tableau, ligne, colonne, p);
+        int i = 1;
+        while (i > 0) {
+            TimeUnit.MILLISECONDS.sleep(120);
+            //le jeu est le même qu'au tour précédent
+            if(Arrays.deepEquals(tableau,nouvelEtat(tableau, ligne, colonne))){
+                System.out.println("Le jeu est figé.");
+                break;
+            }
+            else tableau = nouvelEtat(tableau, ligne, colonne);
+            Afficher(tableau, ligne, colonne, p);
         }
     }
 
-    void tour(int[] self) {
-        /*Met à jour toutes les cellules du tableau en respectant les règles du jeu de la vie.*/
+
+    //afficher la matrice (dans le terminal)
+    static void afficherTableau(int tableau[][], int ligne, int colonne) {
+        System.out.println("Etat des cellules");
+        for (int i = 0; i < ligne; i++) {
+            for (int j = 0; j < colonne; j++) {
+                if (tableau[i][j] == 0) //mort
+                    System.out.print(".");
+                else //vie
+                    System.out.print("*");
+            }
+            System.out.println(); //ligne suivante
+        }
     }
 
-    void valeurCase(int[] self, int i, int j) {
-        /*renvoie la valeur de la case [i][j] ou 0 si la case n'existe pas.*/
+    //renvoie la somme des valeurs des voisins vivants sur la case [i][j].
+    static int totalVoisins(int[][] tableau, int i, int j) {
+        int voisins = 0;
+        for(int l =-1; l<2; l++){
+            for(int c=-1; c<2; c++){
+                if(l != 0 || c !=0){
+                    if ( i+l >=0 && i+l < tableau.length &&
+                            j+c >=0 && j+c < tableau.length &&
+                            tableau[i + l][j + c] == 1) {
+                        voisins += 1;
+                    }
+                }
+            }
+        }
+        return voisins;
     }
 
-    void totalVoisins(int[] self, int i, int j) {
-        /*renvoie la somme des valeurs des voisins sur la case [i][j].*/
+    //renvoie le nouvel état de la cellule tableau[i][j]
+    static int resultatVoisin(int[][] tableau, int i, int j){
+        //cellule morte ou vivante avec 3 voisins en vie
+        if(totalVoisins(tableau,i,j) == 3){
+            return 1;
+        }
+        //cellule en vie avec 2 voisins en vie
+        if(tableau[i][j] == 1 && totalVoisins(tableau,i,j) ==2){
+            return 1;
+        }
+        return 0 ;
     }
 
-    void resultat(int[] self, int valeurCase, int totalVoisins) {
-        /*Renvoie la valeur suivante d'une la cellule
-        :param valeurCase: la valeur de la cellule (0 ou 1)
-        :param totalVoisins: la somme des valeurs des voisins
-        :return: la valeur de la cellule au tour suivant
-        >>> a= JeuDeLaVie([])
-        >>> a.resultat(0,3)
-        1
-        >>> a= JeuDelaVie([])
-        >>> a.resultat(0,1)
-        0 */
+    //renvoie le nouveau tableau des états des cellules
+    static int[][] nouvelEtat(int[][] tableau, int ligne, int colonne){
+        int[][] nouveauTableau = new int[ligne][colonne];
+        for(int i=0; i<ligne; i++){
+            for(int j=0; j<colonne; j++){
+                nouveauTableau[i][j] = resultatVoisin(tableau,i,j);
+            }
+        }
+        return nouveauTableau;
     }
+
+    //panneau d'affichage
+    static JFrame panneau(int ligne){
+        JFrame frame=new JFrame("Jeu de la Vie");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return frame;
+    }
+
+    //(majoritairement copié d'internet)
+    static void Afficher(int[][] tableau, int ligne, int colonne, JFrame frame) {
+        JPanel panel = new JPanel(new GridLayout(0,colonne));
+
+        for( int i=0; i<ligne; i++) {
+            for( int j=0; j<colonne; j++) {
+                int valeur = tableau[i][j];
+                JPanel cellule = new JPanel();
+                cellule.setPreferredSize(new Dimension(32,32));
+                // donne une taille de 32x32 pixels par défaut
+                if ( valeur==0 ) {
+                    cellule.setBackground(Color.WHITE);
+                }
+                else {
+                    cellule.setBackground(Color.BLACK);
+                }
+                panel.add(cellule);
+            }
+        }
+
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
